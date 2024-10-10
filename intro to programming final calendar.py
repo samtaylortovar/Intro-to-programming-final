37d613a239897285de4ce78975662f4208150eb8
Drewd2 <Drew.nielsen15@gmail.com>
2024-10-10 13:13:04 -0600

    Update intro to programming final calendar.py

--- intro to programming final calendar.py
+++ intro to programming final calendar.py
@@ -1,1 +1,10 @@
 print("intro to programming final calendar")
+
+import calendar
+import datetime
+
+now = datetime.datetime.now()
+year = now.year
+month = now.month
+
+print(calendar.month(year, month))
